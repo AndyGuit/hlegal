adb0fa46be2b648a7d20ab8e65ca52a8317f36f6
AndyGuit <totalslash@msn.com>
2024-02-02 03:31:59 +0200

    feat: add scrolling tab links on small widths

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const tabsNavEl = document.querySelector('.nav-tabs');
+const tabsNavLinkEl = tabsNavEl.querySelectorAll('.nav-link');
+
+// scroll tab links on small screens
+tabsNavLinkEl.forEach((link, index) => {
+  link.addEventListener('click', () => {
+    if (screen.width >= 992) return;
+
+    let computedWidth = 0;
+
+    for (let i = 0; i < index; i++) {
+      computedWidth += tabsNavLinkEl[i].getBoundingClientRect().width;
+    }
+
+    tabsNavEl.style = `transform: translateX(-${computedWidth}px)`;
+    console.log('clicked: ', link);
+  });
+});
